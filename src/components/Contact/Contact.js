858a834e38061237a6d78668b1c32c2a6d728501
kamlesh-bharat-raval <dc7798967@gmail.com>
2025-12-11 16:13:39 +0530

    feat: Final contact.js functionality and theme toggle styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -20,6 +20,7 @@ function Contact() {
     e.preventDefault();
 
     try {
+    
       await emailjs.send(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
